c772d05fc6c24dfb9bfc81598afb17a003956217
Benjamin Ledoux <bledoux2002@gmail.com>
2025-12-28 17:21:47 -0600

    fixed damage, raycast was too low

--- Assets/Scripts/EnemyCombat.cs
+++ Assets/Scripts/EnemyCombat.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEngine.UI.Image;
 
 public class EnemyCombat : MonoBehaviour
 {
@@ -7,6 +8,7 @@ public class EnemyCombat : MonoBehaviour
     public int damage = 10;
     public float fireRate = 2f;
     private float nextAttackTime = 0f;
+    public LayerMask playerLayerMask;
 
     public void Attack()
     {
@@ -17,11 +19,12 @@ public class EnemyCombat : MonoBehaviour
 
         FireAudio.Play();
 
-        if (Physics.Raycast(transform.position,
+        if (Physics.Raycast(transform.position + Vector3.up,
                              transform.forward,
                              out RaycastHit hit,
-                             100f))
+                             100f, playerLayerMask))
         {
+            Debug.Log("Hit");
             hit.collider.GetComponent<HealthManager>()?.TakeDamage(damage);
         }
     }
